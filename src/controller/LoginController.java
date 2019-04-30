9001736b82d2c4d805c38642b86de38c34965f65
Micah Killick <killickster@gmail.com>
2019-04-29 18:45:10 -0600

    continued to work on controllers

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,4 +1,76 @@
 package controller;
 
-public class Controller {
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
+
+
+    @FXML
+    private Button forgotPasswordButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private CheckBox rememberMeBox;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        username.setStyle("-fx-text-inner-color: #a0a2ab");
+        passwordField.setStyle("-fx-text-inner-color: #a0a2ab");
+
+    }
+
+    @FXML
+    public void signUpAction(ActionEvent e) throws IOException {
+
+        Stage stage = new Stage();
+
+        loginButton.getScene().getWindow().hide();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/SignUp.fxml"));
+
+        Scene scene = new Scene(root, 600, 300);
+
+        stage.setScene(scene);
+
+        stage.show();
+
+
+    }
+
+    @FXML
+    public void loginAction(ActionEvent e) throws IOException{
+
+        System.out.print("Logged in successfuly");
+
+    }
+
+
 }
--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,4 +1,78 @@
 package controller;
 
-public class SignUpController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+
+
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField locationField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private RadioButton maleSelector;
+
+    @FXML
+    private RadioButton femaleSelector;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        usernameField.setStyle("-fx-text-inner-color: #a0a2ab");
+        passwordField.setStyle("-fx-text-inner-color: #a0a2ab");
+        locationField.setStyle("-fx-text-inner-color: #a0a2ab");
+
+    }
+
+    public void signUpAction(ActionEvent e1) throws IOException{
+
+        signUpButton.getScene().getWindow().hide();
+
+        Stage stage = new Stage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginMain.fxml"));
+
+        Scene scene = new Scene(root, 600,300);
+
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
+
+    public void loginAction(ActionEvent e2){
+
+
+
+    }
+
+
 }
